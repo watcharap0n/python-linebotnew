40dfd170834034040ef68e1ce2b6110ca94240fd
watcharap0n <wera.watcharapon@gmail.com>
2020-09-27 13:32:22 +0700

    Update firebase_auth.py

--- firebase_auth.py
+++ firebase_auth.py
@@ -19,10 +19,10 @@ from linebot.models import (MessageEvent, TextMessage, TextSendMessage,
                             BaseSize, URIImagemapAction, ImagemapArea)
 from mangoerp.mangoerp_card import mangoerp
 
-cred = credentials.Certificate('config/authen_firebase.json')
-firebase_auth = firebase_admin.initialize_app(cred)
+# cred = credentials.Certificate('config/authen_firebase.json')
+# firebase_auth = firebase_admin.initialize_app(cred)
 
-with open('config/database_test/firebase.json', encoding='utf8') as json_file:
+with open('config/database_older/firebase.json', encoding='utf8') as json_file:
     data = json.load(json_file)
     config = data['firebase']
     firebase = pyrebase.initialize_app(config)
@@ -33,30 +33,48 @@ with open('config/database_test/firebase.json', encoding='utf8') as json_file:
 
 app = Flask(__name__)
 
-@app.route('/api/signup')
-def signup():
-    email = request.form.get('email')
-    password = request.form.get('password')
-    if email is None or password is None:
-        return {'message': 'Error missing email or password'},400
-    try:
-        user = auth.create_user(
-            email=email,
-            password=password
-        )
-        day = datetime.today().day
-        month = datetime.today().month
-        second = datetime.today().second
-        minute = datetime.today().minute
-        hour = datetime.today().hour
-        year = datetime.today().year
-        data = {'email': user.email, 'userToken': user.uid,
-                'Datetime': {'day': day, 'month': month, 'year': year, 'hour': hour, 'minute': minute, 'second': second}}
-        db.child('id').push(data)
-        return {'message': f'Successfully created user {user.email}'}, 200
-    except:
-        return {'message': 'Error creating user'}, 400
+embedding = []
+answers = []
+xtrain = []
+datasets = db.child('transaction_mango').child('datasets').get()
+count = 0
+for d in datasets.each():
+    if d.val():
+        txt = ''
+        answers.append(d.val()['answers'])
+        if d.val()['questions']:
+            for i in d.val()['questions']:
+                txt = txt + i
+            xtrain.append(txt)
+            embedding.append(count)
+            count += 1
 
+count_vect = CountVectorizer(tokenizer=tokenize)
+Xtrain_count = count_vect.fit_transform(xtrain)
+tf_transformer = TfidfTransformer(use_idf=False)
+tf_transformer.fit(Xtrain_count)
+Xtrain_tf = tf_transformer.transform(Xtrain_count)
+SVM = svm.SVC(C=1.0, kernel='linear', degree=3,
+              gamma='auto', probability=True)
+SVM.fit(Xtrain_tf, embedding)
+msg = ['หักเงิน']
+Xtest_count = count_vect.transform(msg)
+Xtest_tf = tf_transformer.transform(Xtest_count)
+label = SVM.predict(Xtest_tf)
+prop = SVM.predict_proba(Xtest_tf)[0][label]
+p = float(prop)
+confidence = (0.3565152559 / ((len(embedding) * p) ** 0.5)) ** 2
+print(label)
+print(answers[5])
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5003)
+x = random.choice(answers[int(label)])
+
+while True:
+    if x == '':
+        x = random.choice(answers[int(label)])
+        print(x)
+        pass
+    else:
+        x = random.choice(answers[int(label)])
+        print(x)
+        break
