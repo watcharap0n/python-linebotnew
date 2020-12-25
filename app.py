9dd649fd66cffd625d7152da9f2d4ca635371c0b
watcharapon weeraborirak <s5919410022@sau.ac.th>
2020-12-25 13:42:14 +0700

    Update app.py

--- app.py
+++ app.py
@@ -115,7 +115,7 @@ def sessionCustomer(user, password):
 @app.before_request
 def make_session_permanent():
     session.permanent = True
-    app.permanent_session_lifetime = timedelta(minutes=COOKIE_TIME_OUT)
+    app.permanent_session_lifetime = timedelta(minutes=60)
 
 
 @app.route('/test', methods=['GET', 'POST'])
